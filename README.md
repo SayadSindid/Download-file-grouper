f55635d1a2b583d203a89c0f00a6e24146ffb61b
SayadSindid <SayadSindid@proton.me>
2025-06-05 19:23:34 +0200

    Refined the prompting function, started folder watching implementation

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # DOWNLOAD FILE GROUPER
 
-- TODO: Implement the caching of the download folder directory in order to use it as a background script everytime the script is launched  
+**TODO:**  
+- Implement the caching of the download folder directory in order to use it as a background script everytime the script is launched  
+- Make the process a background script once the user has answered all the prompts
 
-
-Additional features : Implement choice of which file extension you want to group
-
+**Additional features:**  
+- Implement choice of which file extension you want to group  
+- Add a log text file of all the file that were moved to which folder  
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,40 @@
-import path from 'node:path';
-import * as fs from 'node:fs/promises';
+import * as fs from 'node:fs';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import { existsSync } from 'node:fs';
 
 
 console.log("Starting...");
 
 type linkType = string;
 
+// New class thingy to manipulate Input/Output
+const rl = readline.createInterface({ input, output });
 
-async function promptDownloadFolder(retriesCounter: number = 0) {
-    let newLink:linkType = "";
+
+async function promptingToUser(message: string, assertionCallback: (errorMessage: string) => boolean | Error, retriesCounter: number = 0) {
+    let userResponse:linkType = "";
     
-    // New class thingy to manipulate Input/Output
-    const rl = readline.createInterface({ input, output });
     try {
         // Prompting the user
-        newLink = await rl.question("Download folder location: ");
+        userResponse = await rl.question(message);
         // Removing any white space
-        newLink = newLink.trim();
-        // Must include either / or \ (handling linux and windows path)
-        if (!newLink.includes("/") && !newLink.includes("\\")) {
-            throw new Error("Incorrect download folder link. Please re-type your download folder location");
+        userResponse = userResponse.trim();
+
+        let callbackResponse = assertionCallback(userResponse)
+
+        if (callbackResponse === true) {
+            return userResponse;
         } else {
-            rl.close();
-            return newLink;
+            throw callbackResponse;
         }
+
     } catch (error: unknown) {
-        // Type Guard error, No idea if it's best practice
+        // Type Guard error
         if (error instanceof Error) {
-            // Handle Ctrl+C handling to stop the prompting
-            // Note : I don't know if it's a correct approach to get the abort info from the error.message
+            // Handle Ctrl+C/Abort handling to stop the prompting
             // Add a max retries handling at 40 to not overflow the stack
-            if (error.message === "Aborted with Ctrl+C") {
+            if (error.name === "AbortError") {
+                // Close the interface
                 rl.close();
                 // Exiting the program
                 process.exit();
@@ -46,9 +47,9 @@ async function promptDownloadFolder(retriesCounter: number = 0) {
             }
             
             console.log(error.message);
-            rl.close();
             // Recursively call the prompting function
-            return await promptDownloadFolder(retriesCounter + 1);
+            // The callback is re-called and re-asigned anyway
+            return await promptingToUser(message, assertionCallback, retriesCounter + 1);
         } else {
             // Handling unregistered Error
             console.log(error);
@@ -58,45 +59,64 @@ async function promptDownloadFolder(retriesCounter: number = 0) {
     }
 }
 
-const downloadFolderLink: linkType = await promptDownloadFolder();
+async function promptDownloadFolder() {
 
-async function readDirectory(folderLink: linkType) {
-        
-    try {
-        let files: Array<string> = await fs.readdir(folderLink)
-        console.log(files);
-        return files;
-    } catch (error) {
-        if (error instanceof Error) {
-            // TODO: Need proper message error handling
-            console.log(error.message);
-            process.exit();
+    // The callback manage the validation condition and the error throwing
+    return await promptingToUser("Download folder location:", function(userMessage: string) {
+        // Must include either / or \ (handling linux and windows path)
+        if (!userMessage.includes("/") && !userMessage.includes("\\")) {
+            return new Error("Incorrect download folder link. Please re-type your download folder location");
         } else {
-            // Handling unregistered Error
-            console.log(error);
-            console.log("Unregistered Error, exiting the program.");
-            process.exit();
+            return true;
         }
-    }
+    })
+
 }
 
-const directoryContentDownloadFolder: Array<string> = await readDirectory(downloadFolderLink);
+async function promptFolderChoice() {
 
-
-async function createNewFolders(folderLink: string) {
     // Data = XML, JSON, YAML ect..
     // Subtitles = Ass, Srt ect...
     // Executable = bat, exe, sh, wsf ect..
     // Text = pdf, txt, doc, odt ect...
-    const folderList = ["Pictures", "Sounds", "Data", "Compressed", "Subtitles", "Executable", "Font", "Video", "Text"]
+    let folderList = ["Pictures", "Sounds", "Data", "Compressed", "Subtitles", "Executable", "Font", "Video", "Text"];
+    const validationRegex = /[y, n]/mi;
+    let value: string;
 
+    for (let i = 0; i < folderList.length; i++) {
+        value = await promptingToUser(`Create ${folderList[i]} folder? (y/n)`, function(userMessage: string) {
+            // Must include either y or n
+            if (!userMessage.toLowerCase().match(validationRegex)) {
+                return new Error("Incorrect input. Please type either y or n");
+            } else {
+                return true;
+            }
+        })
+
+        if (value === "y") {
+            // TODO: Add message "This folder will be created" or nothing
+            console.log("gg");
+        } else {
+            // Note to self: The index need to be decremanted because
+            // everything is shifted to the left when splicing an element.
+            folderList.splice(i, 1);
+            i--;
+        }
+    }
+
+    return folderList
+}
+
+function createNewFolders(grouperFolder: string, folders: Array<string>) {
 
     // Create all directory
-    console.log("Creating directory...");
-    for (let i = 0; i < folderList.length; i++) {
-        if (existsSync(`${folderLink}/${folderList[i]}`)) {
-            await fs.mkdir(folderList[i]);
+    console.log("Creating directories...");
+    for (let i = 0; i < folders.length; i++) {
+        if (!fs.existsSync(`${grouperFolder}/${folders[i]}`)) {
+            fs.mkdirSync(`${grouperFolder}/${folders[i]}`);
+            console.log(`Created ${folders[i]} folder.`);
         } else {
+            console.log(`${folders[i]} folder already exist.`);
             continue;
         }
     }
@@ -104,8 +124,25 @@ async function createNewFolders(folderLink: string) {
     
 }
 
+const grouperFolderLink: linkType = await promptDownloadFolder();
+const userNewFolders = await promptFolderChoice();
+createNewFolders(grouperFolderLink, userNewFolders);
+rl.close()
 
-
+fs.watch(grouperFolderLink, (eventType, filename) => {
+    console.log(`event type is: ${eventType}`);
+    if (filename) {
+        // TODO: Implement a check if either the file exist or not before trying to move it
+        // TODO2:Get the extension of the file if it's among the extension I watch move to corresponding folder
+        // TODO3: Regine the regex so the dot is not in the resulting string
+        console.log(`filename provided: ${filename}`);
+        if (filename.match(/\.[0-9a-z]+$/i) !== null) {
+            console.log(filename.match(/\.[0-9a-z]+$/i)!.join(""));
+        }
+    } else {
+        console.log('filename not provided');
+    }
+}); 
 
 
 
