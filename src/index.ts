c9a02db5f79eaca490b9c400008f77d879346a87
SayadSindid <SayadSindid@proton.me>
2025-06-07 00:43:07 +0200

    starting existing file grouping function

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,43 @@ console.log("Starting...");
 
 type linkType = string;
 
+// Dynamic key type necessary otherwise IDE error
+type processedObjectType = {
+    [key: string]: string;
+};
+
 // New class thingy to manipulate Input/Output
 const rl = readline.createInterface({ input, output });
 
+// y or n
+const validationRegex = /[y, n]/mi;
+
+const folderExtension = {
+    "Pictures": ["jpg", "jpeg", "png", "bmp", "wepb", "svg", "ico", "psd", "ai", "eps"],
+    "Sounds": ["mid", "midi", "mp3", "ogg", "wav", "flac", "aac", "ogg", "m4a", "opus"],
+    "Data": ["csv", "json", "xml", "yaml", "sql", "db", "sqlite", "xls", "xlsx", "ods", "mdb", "dat"],
+    "Compressed": ["zip", "rar", "7z", "tar", "gz", "bz2", "xz", "tar.gz", "tar.bz2"],
+    "Subtitles": ["srt", "ass", "ssa", "vtt", "sub"],
+    "Executable": ["exe", "msi", "app", "deb", "bin", "bat", "sh", "cmd", "rpm", "wsf"],
+    "Font": ["ttf", "otf", "woff", "woff2", "eot", "fon"],
+    "Video": ["gif", "mkv", "mp4", "avi", "mov", "wmv", "flv", "webm", "m4v", "3gp"],
+    "Text": ["txt", "rtf", "doc", "docx", "odt", "tex", "md", "rst", "log", "pdf"],
+}
+
+function processedFolderExtension() {
+    let newFolderExtension: processedObjectType = {};
+
+    for (const [key, value] of Object.entries(folderExtension)) {
+        for (let i = 0; i < value.length; i++) {
+            Object.assign(newFolderExtension, {[value[i]]: key});
+        }
+    }
+
+    return newFolderExtension;
+}
+
+// Data structure transformaton
+const ProcessedFolder = processedFolderExtension();
 
 async function promptingToUser(message: string, assertionCallback: (errorMessage: string) => boolean | Error, retriesCounter: number = 0) {
     let userResponse:linkType = "";
@@ -75,12 +109,7 @@ async function promptDownloadFolder() {
 
 async function promptFolderChoice() {
 
-    // Data = XML, JSON, YAML ect..
-    // Subtitles = Ass, Srt ect...
-    // Executable = bat, exe, sh, wsf ect..
-    // Text = pdf, txt, doc, odt ect...
     let folderList = ["Pictures", "Sounds", "Data", "Compressed", "Subtitles", "Executable", "Font", "Video", "Text"];
-    const validationRegex = /[y, n]/mi;
     let value: string;
 
     for (let i = 0; i < folderList.length; i++) {
@@ -104,7 +133,7 @@ async function promptFolderChoice() {
         }
     }
 
-    return folderList
+    return folderList.length === 0 ? false : folderList;
 }
 
 function createNewFolders(grouperFolder: string, folders: Array<string>) {
@@ -124,25 +153,76 @@ function createNewFolders(grouperFolder: string, folders: Array<string>) {
     
 }
 
+async function promptSortExistingFile(userFolderList: Array<string>) {
+
+    // Check for file with a dot  in the name who is not on the first index
+    // TODO: Refine the regex for edge case like .tar.gz
+    const regexFileExtension = /(?<!^)\.([^.]+)$/gm;
+    
+        let value = await promptingToUser("Do you want to classify all the existing files in the folders you created? (y/n)",function(userMessage: string) {
+            // Must include either y or n
+            if (!userMessage.toLowerCase().match(validationRegex)) {
+                return new Error("Incorrect input. Please type either y or n");
+            } else {
+                return true;
+            }
+        })
+
+        const filesGrouperFolder = fs.readdirSync(grouperFolderLink).filter((x) => x.match(regexFileExtension));
+
+        console.log(filesGrouperFolder);
+        console.log(value);
+
+        // TODO: finish the function with the newly created object ProcessedFolder
+
+        
+        // for (let i = 0; i < filesGrouperFolder.length; i++) {
+            
+        // }
+
+
+
+        // if (value === "y") {
+        //     for (let i = 0; i < userFolderList.length; i++) {
+        //         // Loop over
+        //         for (let i = 0; i < filesGrouperFolder.length; i++) {
+        //             filesGrouperFolder[i];
+                    
+        //         }
+                
+        //     }
+        // } else {
+        //     return null;
+        // }
+
+}
+
 const grouperFolderLink: linkType = await promptDownloadFolder();
 const userNewFolders = await promptFolderChoice();
-createNewFolders(grouperFolderLink, userNewFolders);
+
+if (!userNewFolders) {
+    console.log("No new folders will be created.");
+} else {
+    createNewFolders(grouperFolderLink, userNewFolders);
+    await promptSortExistingFile(userNewFolders);
+}
+
 rl.close()
 
-fs.watch(grouperFolderLink, (eventType, filename) => {
-    console.log(`event type is: ${eventType}`);
-    if (filename) {
-        // TODO: Implement a check if either the file exist or not before trying to move it
-        // TODO2:Get the extension of the file if it's among the extension I watch move to corresponding folder
-        // TODO3: Regine the regex so the dot is not in the resulting string
-        console.log(`filename provided: ${filename}`);
-        if (filename.match(/\.[0-9a-z]+$/i) !== null) {
-            console.log(filename.match(/\.[0-9a-z]+$/i)!.join(""));
-        }
-    } else {
-        console.log('filename not provided');
-    }
-}); 
+// fs.watch(grouperFolderLink, (eventType, filename) => {
+//     console.log(`event type is: ${eventType}`);
+//     if (filename) {
+//         // TODO: Implement a check if weither the file exist or not before trying to move it
+//         // TODO2:Get the extension of the file if it's among the extension I watch move to corresponding folder
+//         // TODO3: Regine the regex so the dot is not in the resulting string
+//         console.log(`filename provided: ${filename}`);
+//         if (filename.match(/\.[0-9a-z]+$/i) !== null) {
+//             console.log(filename.match(/\.[0-9a-z]+$/i)!.join(""));
+//         }
+//     } else {
+//         console.log('filename not provided');
+//     }
+// }); 
 
 
 
